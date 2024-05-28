da5e524d1f7d454efee9067de8c34e496dcb11a9
Brice Kelly <ntirukelly@gmail.com>
2024-05-28 12:23:24 +0200

    new-changes

--- src/components/DashboardStatsGrid.jsx
+++ src/components/DashboardStatsGrid.jsx
@@ -9,7 +9,7 @@ export default function DashboardStatsGrid() {
 					<IoBagHandle className="text-2xl text-white" />
 				</div>
 				<div className="pl-4">
-					<span className="text-sm text-gray-500 font-light">Total donars</span>
+					<span className="text-sm text-gray-500 font-light">Total Bookings</span>
 					<div className="flex items-center">
 						<strong className="text-xl text-gray-700 font-semibold">232</strong>
 						<span className="text-sm text-green-500 pl-2">+343</span>
@@ -33,7 +33,7 @@ export default function DashboardStatsGrid() {
 					<IoPeople className="text-2xl text-white" />
 				</div>
 				<div className="pl-4">
-					<span className="text-sm text-gray-500 font-light">Total hosiptals</span>
+					<span className="text-sm text-gray-500 font-light">Total Tours</span>
 					<div className="flex items-center">
 						<strong className="text-xl text-gray-700 font-semibold">313</strong>
 						<span className="text-sm text-red-500 pl-2">-30</span>
